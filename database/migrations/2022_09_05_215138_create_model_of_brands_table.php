96b0b9403767c56f01d902f62ae5b0e3e7fb6417
Daniel González <daniel1992gonzalez@gmail.com>
2022-09-05 18:09:10 -0400

    edit migrations

--- database/migrations/2022_09_05_215138_create_model_of_brands_table.php
+++ database/migrations/2022_09_05_215138_create_model_of_brands_table.php
@@ -15,6 +15,9 @@ class CreateModelOfBrandsTable extends Migration
     {
         Schema::create('model_of_brands', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('brand_id');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
--- database/migrations/2022_09_05_215239_create_cars_table.php
+++ database/migrations/2022_09_05_215239_create_cars_table.php
@@ -15,6 +15,12 @@ class CreateCarsTable extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('model_of_brand_id');
+            $table->foreign('model_of_brand_id')->references('id')->on('model_of_brands');
+            $table->string('plate', 255);
+            $table->year('year');
+            $table->string('color', 255);
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
