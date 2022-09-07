3aaaea8ef4d04a9db93110c5ba68c68e26064d26
Daniel González <daniel1992gonzalez@gmail.com>
2022-09-07 09:33:23 -0400

    resource structure modification

---
+++ app/Http/Resources/CarResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'model_of_brand_id' => $this->model_of_brand_id,
+            'plate' => $this->plate,
+            'year' => $this->year,
+            'color' => $this->color,
+            'status' => $this->status,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
