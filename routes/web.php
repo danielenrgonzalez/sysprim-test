c279aa4e34410f049b9d1ac6eb15366cf9cb472e
Daniel González <daniel1992gonzalez@gmail.com>
2022-09-07 09:35:44 -0400

    api and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index')->name('home.index');
+    Route::get('/regitrar_vehiculo', 'create')->name('home.create');
+    Route::get('/detalle_vehiculo/{car_id}', 'show')->name('home.show');
 });
+
+
